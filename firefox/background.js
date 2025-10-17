303213bf90ee4a50805ccf948382b8ff984e4852
bacloud22 <130276583+bacloud22@users.noreply.github.com>
2025-10-17 21:42:49 +0200

    fix counter initial value in case of iframes

--- firefox/background.js
+++ firefox/background.js
@@ -1,5 +1,15 @@
 browser.webRequest.onBeforeRequest.addListener(
-  function () { return { cancel: true }; },
-  { urls: ["*://accounts.google.com/gsi/iframe/select*", "*://accounts.google.com/gsi/client*"] },
+  async function () {
+    // Increment counter when blocking
+    const { blockedCount = 0 } = await browser.storage.local.get('blockedCount');
+    await browser.storage.local.set({ blockedCount: blockedCount + 1 });
+    return { cancel: true };
+  },
+  {
+    urls: [
+      "*://accounts.google.com/gsi/iframe/select*",
+      "*://accounts.google.com/gsi/client*"
+    ]
+  },
   ["blocking"]
 );
